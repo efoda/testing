1a4b1bbb0bad6c74be23076f5e0062953e50935b
Engy Fouda <efoda@ieee.org>
2018-07-17 18:57:19 -0400

    Add files via upload

--- src/hitcount.py
+++ src/hitcount.py
@@ -1,9 +1,11 @@
 import redis
-
+# connect to redis server
 r = redis.StrictRedis(host='0.0.0.0', port=6379, db=0)
 
-def hit(key):
-    r.incr(key)
+# increase the hit count for the usr
+def hit(usr):
+    r.incr(usr)
 
-def getHit(key):
-    return (r.get(key))
+# get the hit count for the usr
+def getHit(usr):
+    return (r.get(usr))
